690e6b4c65ea0d50376648da45b079aebed84b54
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2025-02-11 19:53:45 -0500

    simple framework

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
 # f_ui_design_template
 
-A new Flutter project.
+Base project template for a UI design project with no logic
 
-## Getting Started
+Demo: https://youtube.com/shorts/v6nlkfl0w5g   
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
--- lib/ui/pages/flutter_course_page.dart
+++ lib/ui/pages/flutter_course_page.dart
@@ -5,6 +5,33 @@ class FlutterCoursePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: SafeArea(
+      // you may remove this widget if you don't want to use it
+      child: Column(
+        children: <Widget>[
+          buildHeader(),
+          buildCourseFocus(),
+          buildModules(),
+          buildProjectList(),
+        ],
+      ),
+    ));
+  }
+
+  Widget buildHeader() {
+    return const Text("Flutter course");
+  }
+
+  Widget buildCourseFocus() {
+    return const Text("Focus on the course");
+  }
+
+  Widget buildModules() {
+    return const Text("Modules");
+  }
+
+  Widget buildProjectList() {
+    return const Text("Projects");
   }
 }
