8661875a8abc6ddabc5b5240a103419b8a5bee39
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2025-02-11 19:42:38 -0500

    first commit

---
+++ lib/ui/pages/flutter_course_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FlutterCoursePage extends StatelessWidget {
+  const FlutterCoursePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
